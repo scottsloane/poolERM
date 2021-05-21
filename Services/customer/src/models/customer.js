522317b027e3cad575f76e8ee6dcd80a0a6a7582
Scott Sloane <scott@scottsloane.com>
2021-05-21 13:30:58 -0700

    update customer model schema

--- Services/customer/src/models/customer.js
+++ Services/customer/src/models/customer.js
@@ -1,9 +1,39 @@
 module.exports = () => {
 
+    const _data = {
+        _id : null,
+        name : {
+            first : null,
+            middle: null,
+            last: null
+        },
+        address : {
+            service: {
+                street: null,
+                street2: null,
+                city: null,
+                state: null,
+                zip: null,
+                geo: {
+                    lat: null,
+                    lon: null
+                }
+            },
+            billing : {
+                same: true,
+                street: null,
+                street2: null,
+                city: null,
+                state: null,
+                zip: null
+            }
+        }
+    }
+
     const fetch = (id) => {
         return new Promise((resolve, reject)=>{
             return resolve(true)
-        })
+        });
     }
 
     return {
