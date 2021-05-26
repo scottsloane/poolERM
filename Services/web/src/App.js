76bf5f4c059ac7877b8c3ed45fdd5be5484c7c28
Scott Sloane <scott@scottsloane.com>
2021-05-25 21:35:59 -0700

    fix file case

---
+++ Services/web/src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Routes from './Routes';
+import NavigationBar from './components/navigationbar';
+
+
+function App() {
+  return (
+    <Router>
+      <main>
+        <NavigationBar />
+        <Switch>
+          {Routes.map((route) => (
+            <Route exact path={route.path} key={route.path}>
+              <route.component />
+            </Route>
+          ))}
+        </Switch>
+      </main>
+    </Router>
+  
+  );
+}
+
+export default App;
