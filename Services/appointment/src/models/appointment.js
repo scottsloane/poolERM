c2f7ee64a258592c1bc7e28daddbff6f21386390
Scott Sloane <scott@scottsloane.com>
2021-05-27 22:40:33 -0700

    Create basic appointment api functions

--- Services/appointment/src/models/appointment.js
+++ Services/appointment/src/models/appointment.js
@@ -1,18 +1,85 @@
-module.exports = () => {
+const { ObjectID } = require("bson");
 
-    const _data = {
-        _id : null,
+const merge_into = (a, b) => {
+    Object.keys(b).forEach((key, i) => {
+        if (typeof b[key] === 'object' && b[key] !== null) {
+          if(typeof a[key] === 'undefined') a[key] = {};
+            return merge_into(a[key], b[key]);
+        }
+        if (typeof a[key] !== 'undefined') b[key] = a[key];
+    });
+  return b;
+}
+
+module.exports = async (db, _data) => {
+
+    let data = {
+        _id: null,
         customer: null,
-        date: null
+        time : null,
+        tasks : null,
+        actions: null,
+        status : 1
     }
 
-    const fetch = (id) => {
-        return new Promise((resolve, reject)=>{
-            return resolve(true)
+    const fetch = (_id) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if(typeof _id === 'string') _id = ObjectID(_id);
+            }catch(e){
+                return reject(e);
+            }
+            if(typeof _id === 'undefined') {
+                let res = await db.collection('appointment').find({}).toArray();
+                return resolve(res);
+            } else {
+                let res = await db.collection('appointment').findOne({
+                    _id
+                }).catch(err => {
+                    return reject(err);
+                });
+                
+                data = merge_into(res, data);
+                return resolve(data);
+            }
         });
     }
 
+    const update = (_data) => {
+        data = merge_into(_data, data);
+    }
+
+    const save = () => {
+        return new Promise(async (resolve, reject) => {
+            let res = null;
+            if(data._id === null){
+                res = await db.collection('appointment').insertOne(data, {
+                    upsert: true
+                }).catch(err => {
+                    return reject(err);
+                });
+            }else{
+                res = await db.collection('appointment').replaceOne({_id: data._id}, data).catch(err => {
+                    return reject(err);
+                });
+            }
+            
+            return resolve(res.insertedId)
+        });
+    }
+
+    if (typeof _data !== 'undefined') {
+        if (typeof _data === 'object') {
+            data = merge_into(_data, data);
+        } else {
+            await fetch(_data);
+        }
+    }
+
     return {
-        fetch
+        fetch,
+        update,
+        save,
+        data
     }
 }
